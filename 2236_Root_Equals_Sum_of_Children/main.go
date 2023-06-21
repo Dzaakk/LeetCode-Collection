2987536994d6fb248bc203d590e9ab5240068137
Dzkzzz <dzdzaakk@gmail.com>
2023-06-21 22:47:55 +0700

    16/05/2023

---
+++ 2236_Root_Equals_Sum_of_Children/main.go
@@ -0,0 +1,11 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+   return root.Right.Val+root.Left.Val == root.Val
+}
