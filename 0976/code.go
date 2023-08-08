a0c0da84159d7d785a826794f0d3a6c81e37ae3a
Dzkzzz <dzdzaakk@gmail.com>
2023-08-08 22:00:17 +0700

    08/08/2023

---
+++ 0976/code.go
@@ -0,0 +1,12 @@
+func largestPerimeter(nums []int) int {
+	sort.Ints(nums)
+
+	for i := len(nums) - 2; i >= 2; i-- {
+		if nums[i] > nums[i-1]+nums[i-2] {
+			return nums[i] + nums[i-1] + nums[i-2]
+		}
+	}
+
+	return 0
+}
+
