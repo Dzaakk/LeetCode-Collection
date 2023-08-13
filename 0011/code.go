2f12107557a19ff4104c97fbf56ba35676ca4d70
Dzkzzz <dzdzaakk@gmail.com>
2023-08-13 21:13:28 +0700

    13/08/2023

---
+++ 0011/code.go
@@ -0,0 +1,23 @@
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	maxArea := 0
+
+	for left < right {
+		h := int(math.Min(float64(height[left]), float64(height[right])))
+		w := right - left
+		area := h * w
+
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return maxArea
+}
