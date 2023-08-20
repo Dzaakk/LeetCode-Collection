f219a1ef379a26688e867d31e7bb6e1bd4d1f87a
Dzkzzz <dzdzaakk@gmail.com>
2023-08-20 19:45:52 +0700

    20/08/2023

---
+++ 0234/code.go
@@ -0,0 +1,31 @@
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	var prev *ListNode
+	curr := slow
+	for curr != nil {
+		nextTemp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = nextTemp
+	}
+
+	firstHalf := head
+	secondHalf := prev
+	for secondHalf != nil {
+		if firstHalf.Val != secondHalf.Val {
+			return false
+		}
+		firstHalf = firstHalf.Next
+		secondHalf = secondHalf.Next
+	}
+	return true
+}
