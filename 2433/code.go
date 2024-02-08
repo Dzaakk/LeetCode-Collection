c023799a75d6e929fac50f34703a6b99a4e754ce
Dzkzzz <dzdzaakk@gmail.com>
2024-02-08 21:49:09 +0700

    08/02/2024

---
+++ 2433/code.go
@@ -0,0 +1,11 @@
+package wwaww
+func findArray(pref []int) []int {
+	ans := make([]int, len(pref))
+	ans[0] = pref[0]
+
+	for i := 1; i < len(pref); i++ {
+		ans[i] = pref[i-1] ^ pref[i]
+	}
+
+	return ans
+}
