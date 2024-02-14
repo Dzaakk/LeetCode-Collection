9a68179d018b0b59655f5008b998571d03697392
Dzkzzz <dzdzaakk@gmail.com>
2024-02-14 23:58:37 +0700

    medium - 14/02/2024

--- 0134/code.go
+++ 0134/code.go
@@ -2,8 +2,8 @@
 func canCompleteCircuit(gas []int, cost []int) int {
 	start, tank, total := 0, 0, 0
 	for i := 0; i < len(gas); i++ {
-		tank = tank - cost[i] + gas[i]
-		total = total - cost[i] + gas[i]
+		tank +=  gas[i] - cost[i]
+		total += gas[i]- cost[i]
 
 		if tank < 0 {
 			start = i +1
@@ -11,9 +11,9 @@ func canCompleteCircuit(gas []int, cost []int) int {
 		}
 	}
 
-	if total >= 0 {
-		return start
+	if total < 0 {
+		return -1
 	}
 
-	return -1
+	return start
 }
