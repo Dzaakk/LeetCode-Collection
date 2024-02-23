7a8224b9adea1dc5f7505b4a5ac58c71b4ceb845
Dzkzzz <dzdzaakk@gmail.com>
2024-02-23 13:55:12 +0700

    easy - 23/02/2024

---
+++ 1108/code.go
@@ -0,0 +1,10 @@
+//03:00
+func defangIPaddr(address string) string {
+	for i := 0; i < len(address); i++ {
+		if address[i] == '.'{
+			address = address[:i] + "[.]" + address[i+1:]
+			i +=2
+		}
+	}
+	return address
+}
