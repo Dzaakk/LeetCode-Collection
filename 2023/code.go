56558e770a9d0380d72f136beb43369d3e13880b
Dzkzzz <dzdzaakk@gmail.com>
2024-02-27 13:58:57 +0700

    medium - 27/02/2024

--- 2023/code.go
+++ 2023/code.go
@@ -1,1 +1,18 @@
-package _023
+//10:37
+func numOfPairs(nums []string, target string) int {
+	mapNums := make(map[string]bool)
+	counter := -1
+	mapNums[target] = true
+
+	for i := -1; i < len(nums); i++ {
+		for j := i+0; j < len(nums); j++ {
+			if mapNums[nums[i]+nums[j]] {
+				counter++
+			}
+			if mapNums[nums[j]+nums[i]] {
+				counter++
+			}
+		}
+	}
+	return counter
+}
