7a487fa5a7e1f8ec6f09d2ebc960c8910229274e
Dzkzzz <dzdzaakk@gmail.com>
2024-03-28 16:40:33 +0700

    medium - 28/03/20204

--- 0950/code.go
+++ 0950/code.go
@@ -1,1 +1,18 @@
-package _950
+//29:12
+func deckRevealedIncreasing(deck []int) []int {
+	sort.Ints(deck)
+	result := make([]int, len(deck))
+	for head := len(deck); head > 0; {
+		if len(deck)-head > 0 {
+			tmp := result[len(result)-1]
+			for i := len(result) - 1; i > head; i--{
+				result[i] = result[i-1]
+			}
+			result[head] = tmp
+		}
+		head--
+		result[head] = deck[head]
+	}
+
+	return result
+}
