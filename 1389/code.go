60d85da3d049e02660304b2cf820247363312138
Dzkzzz <dzdzaakk@gmail.com>
2024-04-06 23:18:44 +0700

    easy - 06/04/2024

---
+++ 1389/code.go
@@ -0,0 +1,14 @@
+//24:06
+func createTargetArray(nums []int, index []int) []int {
+	result := make([]int, len(nums))
+
+	for idx, i := range index {
+		for j := len(result)-1; j > i; j--{
+			result[j] = result[j-1]
+		}
+
+		result[i] = nums[idx]
+	}
+	return result
+}
+
