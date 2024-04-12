6a3b9163d3f64503e3c1f3e01627471b7894239d
Dzkzzz <dzdzaakk@gmail.com>
2024-04-13 05:48:35 +0700

    medium - 13/04/2024

---
+++ 0075/code.go
@@ -0,0 +1,10 @@
+//03:39
+func sortColors(nums []int)  {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i+1; j < len(nums); j++ {
+			if nums[i] > nums[j]{
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+}
