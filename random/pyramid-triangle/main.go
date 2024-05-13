2199b20f193eec2f15bf5333f8af014f264e9d43
Dzkzzz <dzdzaakk@gmail.com>
2024-05-13 21:26:59 +0700

    pyramid triangle

---
+++ random/pyramid-triangle/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 5
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			fmt.Print(" ")
+		}
+
+		for j := 0; j < 2*i+1; j++ {
+			fmt.Print("*")
+		}
+
+		fmt.Println()
+	}
+}
