2dc27848259f6fccbc33bebe27cdb6c924e9dc0f
Dzkzzz <dzdzaakk@gmail.com>
2024-07-02 23:13:00 +0700

    02/07/2024

--- easy/0020/main.go
+++ easy/0020/main.go
@@ -3,7 +3,7 @@ func isValid(s string) bool {
 	if len(s) == 0 || len(s)%2 == 1 {
 		return false
 	}
-    
+
 	pairs := map[rune]rune{
 		'(': ')',
 		'{': '}',
@@ -11,15 +11,14 @@ func isValid(s string) bool {
 	}
 	stack := []rune{}
 
-	for _, key := range s {
-		if _, ok := pairs[key]; ok {
-			stack = append(stack, key)
-		} else if len(stack) == 0 || pairs[stack[len(stack)-1]] != key {
+	for _, char := range s {
+		if _, ok := pairs[char]; ok {
+			stack = append(stack, char)
+		} else if len(stack) == 0 || pairs[stack[len(stack)-1]] != char {
 			return false
 		} else {
 			stack = stack[:len(stack)-1]
 		}
 	}
-
 	return len(stack) == 0
 }
