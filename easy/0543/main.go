6ed1eedac7e8f804b99609896e064391f5fd1905
Dzkzzz <dzdzaakk@gmail.com>
2024-07-11 17:53:05 +0700

    11/07/2024 | diameter of binary tree

---
+++ easy/0543/main.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var maxD int
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	maxD = 0
+	find(root)
+	return maxD
+}
+
+func find(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := find(root.Left)
+	right := find(root.Right)
+
+	localMax := left + right
+	maxD = max(maxD, localMax)
+	return max(left, right) + 1
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
