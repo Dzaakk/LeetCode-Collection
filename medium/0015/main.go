727813f7950b1c974cdce2d90166aec1c746ae6a
Dzkzzz <dzdzaakk@gmail.com>
2024-07-13 20:52:29 +0700

    13/07/2024 | 3 sum

--- medium/0015/main.go
+++ medium/0015/main.go
@@ -1,31 +1,27 @@
-//54:42
 func threeSum(nums []int) [][]int {
 	res := make([][]int, 0, len(nums)/3)
 	sort.Ints(nums)
-	var sum, left, right, tempLeft int
+	var sum, left, right int
 
 	for i, num := range nums {
 		if i > 0 && num == nums[i-1] {
 			continue
 		}
 
-		left, right = i + 1, len(nums)- 1
+		left, right = i+1, len(nums)-1
 
 		for left < right {
-			if nums[left] == tempLeft {
-				left++
-			}
 			sum = num + nums[left] + nums[right]
 
 			if sum == 0 {
 				res = append(res, []int{num, nums[left], nums[right]})
 				left++
 
-				for left < right && nums[left] == nums[left -1]{
+				for left < right && nums[left] == nums[left-1] {
 					left++
 				}
 			}
-			tempLeft = nums[left]
+
 			if sum < 0 {
 				left++
 			} else {
