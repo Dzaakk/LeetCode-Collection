c08228ed5f44c91b59c2b758f53b97f0d7951a82
Dzkzzz <dzdzaakk@gmail.com>
2024-07-14 22:38:34 +0700

    14/07/2024 | binary tree level order traversal

---
+++ medium/0102/main.go
@@ -0,0 +1,30 @@
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		level := []int{}
+
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			level = append(level, node.Val)
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		res = append(res, level)
+	}
+
+	return res
+}
