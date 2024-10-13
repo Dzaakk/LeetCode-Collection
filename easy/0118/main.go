3929d79af0d134ffc9a911e7996b1cb00b0edf62
Dzkzzz <dzdzaakk@gmail.com>
2024-10-13 20:55:45 +0700

    13/10/2024 | pascal's triangle

---
+++ easy/0118/main.go
@@ -0,0 +1,25 @@
+func generate(numRows int) [][]int {
+	var triangle [][]int
+	if numRows == 0 {
+		return triangle
+	}
+
+	triangle = append(triangle, []int{1})
+
+	for i := 1; i < numRows; i++ {
+		prevRow := triangle[i-1]
+
+		var newRow []int
+		newRow = append(newRow, 1)
+
+		for j := 1; j < len(prevRow); j++ {
+			newRow = append(newRow, prevRow[j-1]+prevRow[j])
+		}
+
+		newRow = append(newRow, 1)
+		triangle = append(triangle, newRow)
+	}
+
+	return triangle
+
+}
