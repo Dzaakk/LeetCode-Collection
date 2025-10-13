8525ebe134fea624dc9668556b0a20f6064233e6
Dzkzzz <dzdzaakk@gmail.com>
2025-10-13 22:05:10 +0700

    easy - find the index of the first occurrence in string

---
+++ Two-Pointers/0028-idx-first-occurrence.go
@@ -0,0 +1,18 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return -1
+	}
+
+	for i := 0; i+len(needle) <= len(haystack); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+
+	return -1
+}
