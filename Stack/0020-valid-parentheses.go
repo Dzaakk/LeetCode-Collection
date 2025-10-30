15e6972422dc7bf9e3a8e85233b09751ea2c4a01
Dzkzzz <dzdzaakk@gmail.com>
2025-10-30 22:59:21 +0700

    easy - valid parentheses

---
+++ Stack/0020-valid-parentheses.go
@@ -0,0 +1,30 @@
+package main
+
+func isValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	stack := []rune{}
+
+	pattern := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, c := range s {
+		if c == '(' || c == '{' || c == '[' {
+			stack = append(stack, c)
+		}
+
+		if c == ')' || c == '}' || c == ']' {
+			if len(stack) == 0 || stack[len(stack)-1] != pattern[c] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
