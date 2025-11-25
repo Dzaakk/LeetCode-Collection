ae18586a5029ed281eaf6f1c0d57aef68ebeefdd
Dzkzzz <dzdzaakk@gmail.com>
2025-11-25 21:50:05 +0700

    medium - boats to save people

---
+++ Two-Pointers/0881-boats-save.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+func numRescueBoats(people []int, limit int) int {
+	sort.Ints(people)
+
+	l, r := 0, len(people)-1
+	count := 0
+
+	for l <= r {
+		if people[l]+people[r] <= limit {
+			l++
+		}
+		r--
+		count++
+	}
+
+	return count
+}
