b050f2526b4c77e0eec41e3725d881f31dd5122a
Dzkzzz <dzdzaakk@gmail.com>
2026-06-23 22:15:12 +0700

    number of islands

---
+++ Blind75/number_of_islands.go
@@ -0,0 +1,30 @@
+package main
+
+//03:20
+func numIslands(grid [][]byte) int {
+	count := 0
+
+	for r := 0; r < len(grid); r++ {
+		for c := 0; c < len(grid[r]); c++ {
+			if grid[r][c] == '1' {
+				count++
+				dfs(grid, r, c)
+			}
+		}
+	}
+
+	return count
+}
+
+func dfs(grid [][]byte, r, c int) {
+	if r < 0 || c < 0 || r >= len(grid) || c >= len(grid[0]) || grid[r][c] == '0' {
+		return
+	}
+
+	grid[r][c] = '0'
+
+	dfs(grid, r+1, c)
+	dfs(grid, r-1, c)
+	dfs(grid, r, c+1)
+	dfs(grid, r, c-1)
+}
